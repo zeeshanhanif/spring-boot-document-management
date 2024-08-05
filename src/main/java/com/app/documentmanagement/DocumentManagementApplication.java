8321ef3e4b724b8c21f03580247a2e1bd78377c2
zeeshanhanif <zeeshanhanif@gmail.com>
2024-08-06 00:35:29 +0500

    Added initial structure

---
+++ src/main/java/com/app/documentmanagement/DocumentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.app.documentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/documentmanagement/DocumentmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.documentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
