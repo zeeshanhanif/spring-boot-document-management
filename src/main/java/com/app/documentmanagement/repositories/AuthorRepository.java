84ba49d7aafa111437c75c7219f300df6bd92be5
zeeshanhanif <zeeshanhanif@gmail.com>
2024-08-09 16:20:04 +0500

    Added custom find methods in AuthorRepository

--- src/main/java/com/app/documentmanagement/repositories/AuthorRepository.java
+++ src/main/java/com/app/documentmanagement/repositories/AuthorRepository.java
@@ -3,7 +3,12 @@ package com.app.documentmanagement.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.app.documentmanagement.entities.Author;
+import java.util.List;
+
 
 public interface AuthorRepository extends JpaRepository<Author,Long> {
     
+    List<Author> findByFirstName(String firstName);
+    List<Author> findByLastName(String lastName);
+    Author findByFirstNameAndLastName(String firstName, String lastName);
 }
