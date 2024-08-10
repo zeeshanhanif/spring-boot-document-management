a8ab843cdb4b3f74b05c1a2cbaf90c47a4fdd247
zeeshanhanif <zeeshanhanif@gmail.com>
2024-08-11 00:38:42 +0500

    Added Swagger for Endpoint documentation. And added annotation for endpoit documentation on AuthorController and DocumentController

--- src/main/java/com/app/documentmanagement/controllers/AuthorController.java
+++ src/main/java/com/app/documentmanagement/controllers/AuthorController.java
@@ -5,6 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.app.documentmanagement.dto.AuthorDTO;
 import com.app.documentmanagement.services.AuthorService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -21,7 +29,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import jakarta.validation.Valid;
 
-
+@Tag(name = "Author", description = "Author Management APIs")
 @RestController
 @RequestMapping("api/authors")
 public class AuthorController {
@@ -31,26 +39,57 @@ public class AuthorController {
     @Autowired
     private AuthorService authorService;
 
+    @Operation(summary = "Get all the Authors", description = "Get all the Authors object. The response is List of Authors object with id, first name, last name and doucments",
+                        tags = { "Get" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Found all the authors", content = { @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = AuthorDTO.class))) }),
+    })
     @GetMapping
     public ResponseEntity<List<AuthorDTO>> getAllAuthors() {
         return ResponseEntity.status(HttpStatus.OK).body(authorService.getAllAuthors());
     }
 
+    @Operation(summary = "Get a author by its id", description = "Get a Author object by specifying its id. The response is a Author object with id, first name, last name and doucments",
+                        tags = { "Get" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Found author with specified id", content = { @Content(schema = @Schema(implementation = AuthorDTO.class), mediaType = "application/json") }),
+        @ApiResponse(responseCode = "404", description = "Author not found", content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/{id}")
     public ResponseEntity<AuthorDTO> getAuthorById(@PathVariable long id) {
         return ResponseEntity.status(HttpStatus.OK).body(authorService.getAuthorById(id));
     }
 
+    @Operation(summary = "Add Author into system", description = "Add a Author object into system. The response is a Author object with id, first name, last name and doucments",
+                        tags = { "Post" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Author add successfully", content = { @Content(schema = @Schema(implementation = AuthorDTO.class), mediaType = "application/json") }),
+        @ApiResponse(responseCode = "400", description = "Authors First name or Last name not provided", content = @Content(mediaType = "application/json")),
+        @ApiResponse(responseCode = "409", description = "Author already exists with similar first and last name", content = @Content(mediaType = "application/json"))
+    })
     @PostMapping
     public ResponseEntity<AuthorDTO> saveAuthor(@Valid @RequestBody AuthorDTO authorDto) {
         return ResponseEntity.status(HttpStatus.OK).body(authorService.saveAuthor(authorDto));
     }
 
+    @Operation(summary = "Update author by id", description = "Update an Author object into system by specifying its id. The response is a Author object with  id and updated first name, last name and doucments",
+                        tags = { "Put" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Author updated successfully", content = { @Content(schema = @Schema(implementation = AuthorDTO.class), mediaType = "application/json") }),
+        @ApiResponse(responseCode = "404", description = "There can be two different erors:<br>1) Author not found with specified id<br>OR<br>2) Document not found, when try to update document of author but that document does not exists in system", content = @Content(mediaType = "application/json")),
+    })
     @PutMapping("/{id}")
     public ResponseEntity<AuthorDTO> updateAuthor(@PathVariable long id, @RequestBody AuthorDTO authorDto) {
         return ResponseEntity.status(HttpStatus.OK).body(authorService.updateAuthor(id, authorDto));
     }
 
+    @Operation(summary = "Delete author by id", description = "Delete Author from the system by specifying its id. The response is a message stating that delete successful or not",
+                        tags = { "Delete" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Author delete successfully", content = { @Content(schema = @Schema(implementation = AuthorDTO.class), mediaType = "application/json") }),
+        @ApiResponse(responseCode = "404", description = "Author not found with specified id", content = @Content(mediaType = "application/json")),
+        @ApiResponse(responseCode = "400", description = "Author cannot be delete if document is attached to author", content = @Content(mediaType = "application/json"))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteAuthorById(@PathVariable long id){
         boolean result = authorService.deleteAuthorById(id);
--- src/main/java/com/app/documentmanagement/controllers/DocumentController.java
+++ src/main/java/com/app/documentmanagement/controllers/DocumentController.java
@@ -13,14 +13,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.documentmanagement.dto.AuthorDTO;
 import com.app.documentmanagement.dto.DocumentDTO;
 import com.app.documentmanagement.services.DocumentService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PutMapping;
 
-
+@Tag(name = "Document", description = "Document Management APIs")
 @RestController
 @RequestMapping("api/documents")
 public class DocumentController {
@@ -28,26 +36,56 @@ public class DocumentController {
     @Autowired
     private DocumentService documentService;
 
+    @Operation(summary = "Get all the Doucments", description = "Get all the Doucments object. The response is List of Doucment object with id, title, body, references and authors",
+                        tags = { "Get" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Found all the documents", content = { @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = DocumentDTO.class))) }),
+    })
     @GetMapping
     public ResponseEntity<List<DocumentDTO>> getAllDocuments() {
         return ResponseEntity.status(HttpStatus.OK).body(documentService.getAllDocuments());
     }
 
+    @Operation(summary = "Get a document by its id", description = "Get a Doucment object by specifying its id. The response is a Document object with id, title, body, references and authors",
+                        tags = { "Get" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Found document with specified id", content = { @Content(schema = @Schema(implementation = DocumentDTO.class), mediaType = "application/json") }),
+        @ApiResponse(responseCode = "404", description = "Document not found", content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/{id}")
     public ResponseEntity<DocumentDTO> getDocumentById(@PathVariable long id) {
         return ResponseEntity.status(HttpStatus.OK).body(documentService.getDocumentById(id));
     }
 
+    @Operation(summary = "Add Document into system", description = "Add a Document object into system. The response is a Document object with id, title, body, references and authors",
+                        tags = { "Post" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Document add successfully", content = { @Content(schema = @Schema(implementation = DocumentDTO.class), mediaType = "application/json") }),
+        @ApiResponse(responseCode = "400", description = "There can be three different erors:<br>1) Title and body must be provided<br>OR<br>2) References must be provided<br>OR<br>3) Authors must be provided", content = @Content(mediaType = "application/json")),
+        @ApiResponse(responseCode = "404", description = "Author that we are assigning to document not fund", content = @Content(mediaType = "application/json"))
+    })
     @PostMapping
     public ResponseEntity<DocumentDTO> saveDocument(@Valid @RequestBody DocumentDTO documentDto) {
         return ResponseEntity.status(HttpStatus.OK).body(documentService.saveDocument(documentDto));
     }
 
+    @Operation(summary = "Update document by id", description = "Update an document object into system by specifying its id. The response is a Document object with  id and updated title, body, references and authors",
+                        tags = { "Put" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Author updated successfully", content = { @Content(schema = @Schema(implementation = AuthorDTO.class), mediaType = "application/json") }),
+        @ApiResponse(responseCode = "404", description = "There can be two different erors:<br>1) Document not found with specified id<br>OR<br>2) Author not found, when try to update author of document but that author does not exists in system", content = @Content(mediaType = "application/json")),
+    })
     @PutMapping("/{id}")
     public ResponseEntity<DocumentDTO> updateDocument(@PathVariable long id, @RequestBody DocumentDTO documentDto) {
         return ResponseEntity.status(HttpStatus.OK).body(documentService.updateDocument(id, documentDto));
     }
 
+    @Operation(summary = "Delete document by id", description = "Delete Document from the system by specifying its id. The response is a message stating that delete successful or not",
+                        tags = { "Delete" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Document delete successfully", content = { @Content(schema = @Schema(implementation = AuthorDTO.class), mediaType = "application/json") }),
+        @ApiResponse(responseCode = "404", description = "Document not found with specified id", content = @Content(mediaType = "application/json"))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteDocumentById(@PathVariable long id){
         boolean result = documentService.deleteDocumentById(id);
