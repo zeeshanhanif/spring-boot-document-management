92f78612dd332c6d031e6a99862275fb0f983adb
zeeshanhanif <zeeshanhanif@gmail.com>
2024-08-12 01:31:19 +0500

    Changed to TestConfiguration annotation

--- src/test/java/com/app/documentmanagement/DocumentmanagementApplicationTests.java
+++ src/test/java/com/app/documentmanagement/DocumentmanagementApplicationTests.java
@@ -3,11 +3,12 @@ package com.app.documentmanagement;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootTest
-@Configuration
+@TestConfiguration
 class DocumentmanagementApplicationTests {
 
 	@Test
